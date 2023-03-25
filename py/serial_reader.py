5050dcb719caa9f0b864157bd54959172c29066d
jackie-mcaninch <jmcaninch@hawk.iit.edu>
2023-03-25 16:24:31 -0500

    updating helper scripts

--- py/serial_reader.py
+++ py/serial_reader.py
@@ -1,19 +1,38 @@
-import serial
+import argparse
 import matplotlib.pyplot as plt
-import sys
+import serial
+import time
 
-# USAGE: python3 py/serial_reader.py <COM_PORT> <NUM_POINTS>
+# USAGE: python3 py/serial_reader.py <COM_PORT> -d <NUM_SECS>
 
-com_port = sys.argv[1]
-num_pts = sys.argv[2] if len(sys.argv) > 2 else 1000
+# read command line arguments
+parser = argparse.ArgumentParser(description="Plot the values received from Moteino over serial.")
+parser.add_argument("COM_PORT", type=str, nargs=1, help="Designated COM port to listen on (i.e. COM5 on Windows)")
+parser.add_argument("-d, --duration", dest="num_secs", type=int, nargs=1, default=5, help="Number of seconds to listen for serial data")
+args = parser.parse_args()
+
+# initialize serial
+com_port = args.com_port[0]
 ser = serial.Serial(com_port, 115200, timeout=1)
+ser.readline()
+
+# start loop
+num_secs = args.num_secs
+start = time.time()
+t = time.time() - start
 data = []
-counter = 0
+datum = 0
+while t < num_secs:
+    # try to get a reading
+    try:
+        datum = float(ser.readline().decode('utf-8').strip())
+    # use last recorded datum if nothing received
+    except ValueError: pass
+    data.append((datum, t))
+    t = time.time() - start
 
-while counter < num_pts:
-    datum = float(ser.readline().decode('utf-8').strip())
-    data.append(datum)
-    counter += 1
-
-plt.plot(range(num_pts), data, color="blue")
+# plot the voltage data received vs time
+plt.xlabel("Time since start (s)")
+plt.ylabel("Voltage")
+plt.plot([t for (d,t) in data], [d for (d,t) in data], color="blue")
 plt.show()
