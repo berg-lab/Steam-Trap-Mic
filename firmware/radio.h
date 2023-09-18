79b4b718f91bd05a0eb8dc172fc115066d544dcc
Ed <edcalix21@gmail.com>
2023-09-18 17:15:39 -0500

    Working radio!

--- firmware/radio.h
+++ firmware/radio.h
@@ -9,19 +9,17 @@
 class CRadio {
   public:
     void initializeRadio();
-    void sendDataPacket(int array_size, long *pre_temp_array, uint8_t error_byte);
+    void sendDataPacket(long pre_temp, uint8_t error_byte);
     void sendConfigPacket(int no_of_attempts);
-    // uint32_t getPreTemp();
-    // uint32_t getPostTemp();
     
   private:
     uint8_t transaction_id;
     void handleIncomingPacket(const unsigned char* raw);
     
     //HARDCODING FOR TESTING ONLY
-    uint8_t GATEWAY_ID = 1;
-    uint16_t NODE_ID = 60; // 999 = node not configured; 1000 = node currently being configured (limit: 1023)
-    uint8_t NETWORK_ID = 10; // 99 = configuration network, 10 = sensor network (up to 255)
+    uint8_t   GATEWAY_ID = 1;
+    uint16_t  NODE_ID    = 60; // 999 = node not configured; 1000 = node currently being configured (limit: 1023)
+    uint8_t   NETWORK_ID = 10; // 99 = configuration network, 10 = sensor network (up to 255)
 
     char ENCRYPT_KEY[17] = "1234123412341234";
     char NODE_TYPE[4] = "STM";
